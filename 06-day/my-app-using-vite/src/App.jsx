0270b3babfc0dd2a7465d4237e02f05f4513fdad
Harshit Vashisth <harshit@Harshits-MacBook-Air.local>
2024-01-18 11:45:52 +0530

    react app using vite

---
+++ 06-day/my-app-using-vite/src/App.jsx
@@ -0,0 +1,21 @@
+import Navbar from "./Navbar";
+import Images from "./Images";
+import React from "react";
+function App() {
+  // return (
+  //   <React.Fragment>
+  //     <h1>App</h1>
+  //     <Navbar />
+  //     <Images />
+  //   </React.Fragment>
+  // );
+
+  return (
+    <>
+      <h1>App</h1>
+      <Navbar />
+      <Images />
+    </>
+  );
+}
+export default App;
---
+++ 06-day/my-app-using-vite/src/Images.jsx
@@ -0,0 +1,4 @@
+function Images() {
+  return <div>Images</div>;
+}
+export default Images;
