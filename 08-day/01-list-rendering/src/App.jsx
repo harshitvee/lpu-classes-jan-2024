630a89625980e9057f06eddfbb41ed321e8cda3a
Harshit Vashisth <harshitvashisth@gmail.com>
2024-01-20 09:11:41 +0530

    add: render list

--- 08-day/01-list-rendering/src/App.jsx
+++ 08-day/01-list-rendering/src/App.jsx
@@ -1,12 +1,32 @@
-import Greeting from "./Greeting";
 function App() {
+  // const items3 = [<h3> one </h3>, <h3> two </h3>, <h3>three</h3>];
+  const taskList = ["teach students", "play cricket", "improve UI"];
+  // const taskListLis = [];
+  // for (let task of taskList) {
+  //   const taskLi = <li>{task}</li>;
+  //   taskListLis.push(taskLi);
+  // }
+
+  // taskList.forEach((task) => {
+  //   const taskLi = <li>{task}</li>;
+  //   taskListLis.push(taskLi);
+  // });
+  // const taskListLis = taskList.map((task) => {
+  //   return <li>{task}</li>;
+  // });
+
+  // implicit
+  // const taskListLis = taskList.map((task) => <li>{task}</li>);
+
   return (
-    <>
+    <div>
       <h1>App</h1>
-      <Greeting firstName="Harshit" lastName="Vashistha" />
-      <Greeting firstName="John" lastName="Doe" />
-      <Greeting firstName="Jane" lastName="Doe" />
-    </>
+      <ul>
+        {taskList.map((task) => (
+          <li>{task}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
