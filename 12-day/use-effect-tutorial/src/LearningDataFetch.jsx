3015cdbb5c86027398029ae4ae0ae9b4c5739cec
Harshit Vashisth <harshitvashisth@gmail.com>
2024-01-25 10:01:19 +0530

    add: error handling

--- 12-day/use-effect-tutorial/src/LearningDataFetch.jsx
+++ 12-day/use-effect-tutorial/src/LearningDataFetch.jsx
@@ -11,15 +11,26 @@ const endpoint = "https://jsonplaceholder.typicode.com/users";
 
 function LearningDataFetch() {
   const [users, setUsers] = useState([]);
+  const [error, setError] = useState("");
   async function getUsers() {
-    const response = await fetch(endpoint);
-    const data = await response.json();
-    // store this inside state
-    setUsers(data); //  component re-render again
+    try {
+      const response = await fetch(endpoint);
+      if (!response.ok) {
+        throw new Error("Something went wrong");
+      }
+      const data = await response.json();
+      // store this inside state
+      setUsers(data); //  component re-render again
+    } catch (error) {
+      setError(error.message);
+    }
   }
   useEffect(() => {
     getUsers();
   }, []);
+  if (error.trim()) {
+    return <h1>{error}</h1>;
+  }
   return (
     <div>
       <ul>
