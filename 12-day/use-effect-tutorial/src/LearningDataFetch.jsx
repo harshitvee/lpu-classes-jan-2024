243b881e7e25e0bc2074c29ea16f6f635d5e3542
Harshit Vashisth <harshitvashisth@gmail.com>
2024-01-25 10:53:24 +0530

    add loading state

--- 12-day/use-effect-tutorial/src/LearningDataFetch.jsx
+++ 12-day/use-effect-tutorial/src/LearningDataFetch.jsx
@@ -1,18 +1,16 @@
 // Data Fetching ?
 // after component mount/ render / load on screen
-
 import { useEffect, useState } from "react";
-
 // useEffect(() => {}, []) // callback runs after initial mount
-
 // data fetching thing --> asked in all the interviews
-
-const endpoint = "https://jsonplaceholder.typicode.com/users";
+const endpoint = "https://jsonplaceholder.typicode.com/userss";
 
 function LearningDataFetch() {
   const [users, setUsers] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   async function getUsers() {
+    setIsLoading(true);
     try {
       const response = await fetch(endpoint);
       if (!response.ok) {
@@ -21,16 +19,22 @@ function LearningDataFetch() {
       const data = await response.json();
       // store this inside state
       setUsers(data); //  component re-render again
+      setIsLoading(false);
     } catch (error) {
+      setIsLoading(false);
       setError(error.message);
     }
   }
   useEffect(() => {
     getUsers();
   }, []);
+  if (isLoading) {
+    return <h1>Loading ..... </h1>;
+  }
   if (error.trim()) {
     return <h1>{error}</h1>;
   }
+
   return (
     <div>
       <ul>
