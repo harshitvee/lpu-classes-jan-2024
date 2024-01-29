e73ad50773561e852e29432a5ab4ee68f778ef8a
Harshit Vashisth <harshitvashisth@gmail.com>
2024-01-29 09:25:48 +0530

    create separate nav component

---
+++ 13-day/rrd-tutorial/src/components/Nav.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+function Nav() {
+  return (
+    <nav
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        width: "1000px",
+      }}
+    >
+      <h2 className="logo">
+        <Link to="/">Codprog</Link>
+      </h2>
+      <ul
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          gap: "2rem",
+        }}
+      >
+        <li>
+          <Link to="about">About</Link>
+        </li>
+        <li>
+          <Link to="contact">Contact</Link>
+        </li>
+        <li>
+          <Link to="courses">Courses</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+export default Nav;
--- 13-day/rrd-tutorial/src/pages/Layout.jsx
+++ 13-day/rrd-tutorial/src/pages/Layout.jsx
@@ -1,36 +1,9 @@
-import { Outlet, Link } from "react-router-dom";
-
+import { Outlet } from "react-router-dom";
+import Nav from "../components/Nav";
 function Layout() {
   return (
     <>
-      <nav
-        style={{
-          display: "flex",
-          justifyContent: "space-between",
-          width: "1000px",
-        }}
-      >
-        <h2 className="logo">
-          <Link to="/">Codprog</Link>
-        </h2>
-        <ul
-          style={{
-            display: "flex",
-            justifyContent: "space-between",
-            gap: "2rem",
-          }}
-        >
-          <li>
-            <Link to="about">About</Link>
-          </li>
-          <li>
-            <Link to="contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="courses">Courses</Link>
-          </li>
-        </ul>
-      </nav>
+      <Nav />
       <Outlet />
     </>
   );
