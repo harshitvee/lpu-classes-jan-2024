c6a997e363b435ed76d22fac0cee81197fa1d120
Harshit Vashisth <harshitvashisth@gmail.com>
2024-02-02 10:54:39 +0530

    patch and delete req

--- 17-day/backend/index.js
+++ 17-day/backend/index.js
@@ -50,6 +50,56 @@ app.post("/api/v1/todos", (req, res) => {
   }
 });
 
+// update
+
+// title , completed, both
+app.patch("/api/v1/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const todo = todos.find((todo) => todo.id === id);
+  if (todo) {
+    const newTodo = {
+      ...todo,
+      title: "title" in req.body ? req.body.title : todo.title,
+      completed: "completed" in req.body ? req.body.completed : todo.completed,
+    };
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return newTodo;
+      } else {
+        return todo;
+      }
+    });
+    fs.writeFile("db.json", JSON.stringify(updatedTodos), "utf-8", (err) => {
+      if (err) {
+        res.status(500).json({ message: "internal server error" });
+      } else {
+        res.status(200).json(newTodo);
+      }
+    });
+  } else {
+    res.status(404).json({ message: `Todo not found with id ${id}` });
+  }
+});
+
+// delete
+
+app.delete("/api/v1/todos/:id", (req, res) => {
+  const id = req.params.id;
+  const todo = todos.find((todo) => todo.id === id);
+  if (todo) {
+    const updatedTodos = todos.filter((todo) => todo.id !== id);
+    fs.writeFile("db.json", JSON.stringify(updatedTodos), "utf-8", (err) => {
+      if (err) {
+        res.status(500).json({ message: "internal server error" });
+      } else {
+        res.status(200).json({ message: `Todo deleted with id ${id}` });
+      }
+    });
+  } else {
+    res.status(404).json({ message: `Todo with id ${id} does not exist` });
+  }
+});
+
 app.listen(port, () => {
   console.log(`App is running on the port ${port}`);
 });
