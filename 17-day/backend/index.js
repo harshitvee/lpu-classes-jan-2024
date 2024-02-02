6b8b2a6a5b123df8180c5792b5a8bf1c8786af8e
Harshit Vashisth <harshitvashisth@gmail.com>
2024-02-02 10:01:51 +0530

    create new todo

--- 17-day/backend/index.js
+++ 17-day/backend/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const todos = require("./db.json");
-
+const fs = require("fs");
 const app = express();
 const port = 8000;
 
@@ -28,8 +28,26 @@ app.get("/api/v1/todos/:id", (req, res) => {
 
 // create todo
 app.post("/api/v1/todos", (req, res) => {
-  console.log(req.body);
-  res.json({ message: "trying to create todo ? " });
+  if (!("title" in req.body)) {
+    res.status(400).json({ message: "title is missing" });
+  } else if (!("completed" in req.body)) {
+    res.status(400).json({ message: "comleted is missing" });
+  } else {
+    const id = todos.length + 1;
+    const newTodo = {
+      id: id.toString(),
+      title: req.body.title,
+      completed: req.body.completed,
+    };
+    todos.push(newTodo);
+    fs.writeFile("db.json", JSON.stringify(todos), "utf-8", (err) => {
+      if (err) {
+        res.status(500).json({ message: "internal server error" });
+      } else {
+        res.status(200).json(newTodo);
+      }
+    });
+  }
 });
 
 app.listen(port, () => {
