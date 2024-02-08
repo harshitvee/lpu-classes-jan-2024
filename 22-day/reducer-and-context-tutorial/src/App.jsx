1e4c8130abc1a3c8dcdc8df679eb97f1beda4efc
Harshit Vashisth <harshitvashisth@gmail.com>
2024-02-08 09:00:50 +0530

    useReducer tut

---
+++ 22-day/reducer-and-context-tutorial/src/App.jsx
@@ -0,0 +1,12 @@
+import LearningUseReducer from "./LearningUseReducer";
+function App() {
+  return (
+    <div>
+      <LearningUseReducer />
+    </div>
+  );
+}
+export default App;
+
+// useReducer : state management like useState
+// to handle complex state
---
+++ 22-day/reducer-and-context-tutorial/src/LearningUseReducer.jsx
@@ -0,0 +1,63 @@
+// useState
+
+// useReducer
+
+// counter application ? useState
+
+// function LearningUseReducer() {
+//   const [counter, setCounter] = useState(0);
+//   function handleIncrease() {
+//     setCounter((count) => count + 1);
+//   }
+//   function handleDecrease() {
+//     setCounter((count) => count - 1);
+//   }
+//   function handleReset() {
+//     setCounter(0);
+//   }
+//   return (
+//     <div>
+//       <h1>{counter}</h1>
+//       <button onClick={handleIncrease}>Increase </button>
+//       <button onClick={handleReset}>Reset</button>
+//       <button onClick={handleDecrease}>Decrease</button>
+//     </div>
+//   );
+// }
+
+// useReducer
+// input : reducer function , initalState
+
+// output : array with 2 vals [state, function to  update state]
+
+import { useReducer } from "react";
+function reducer(count, action) {
+  if (action.type === "INCREASE") {
+    return count + 1;
+  }
+  if (action.type === "RESET") {
+    return 0;
+  }
+  if (action.type === "DECREASE") {
+    return count - 1;
+  }
+  return count;
+}
+
+// when we want to change the count
+// we call dispatch function
+// dispatch function calls reducer and reducer updates the count
+
+function LearningUseReducer() {
+  const [count, dispatch] = useReducer(reducer, 0);
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch({ type: "INCREASE" })}>Increase</button>
+      <button onClick={() => dispatch({ type: "RESET" })}>Reset</button>
+      <button onClick={() => dispatch({ type: "DECREASE" })}>Decrease</button>
+    </div>
+  );
+}
+export default LearningUseReducer;
