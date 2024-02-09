6120df6db2e0664abc062f756fce4739d4a5f0fd
Harshit Vashisth <harshitvashisth@gmail.com>
2024-02-09 09:44:08 +0530

    add product page

--- 23-day/shopping-cart/src/Components/SingleProduct.jsx
+++ 23-day/shopping-cart/src/Components/SingleProduct.jsx
@@ -1,4 +1,16 @@
+import styles from "./SingleProduct.module.css";
 function SingleProduct({ id, title, category, description, image, price }) {
-  return <div>{title}</div>;
+  return (
+    <div className={styles.singleProduct}>
+      <div className={styles.imgAndDesc}>
+        <img src={image} alt={title} className={styles.image} />
+        <p className={styles.description}>{description}</p>
+      </div>
+      <div className={styles.priceAndBuyBtn}>
+        <h2 className={styles.price}>&#8377;{price}</h2>
+        <button className={styles.addToCartBtn}>Add To Cart</button>
+      </div>
+    </div>
+  );
 }
 export default SingleProduct;
