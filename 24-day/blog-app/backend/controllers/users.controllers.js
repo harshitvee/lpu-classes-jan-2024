2f4ccf16e2ff3ddcac8c9c9c4b0983962b2c87e8
Harshit Vashisth <harshitvashisth@gmail.com>
2024-02-10 11:33:09 +0530

    login and signup functionality

--- 24-day/blog-app/backend/controllers/users.controllers.js
+++ 24-day/blog-app/backend/controllers/users.controllers.js
@@ -1,17 +1,32 @@
 const User = require("../models/User.model");
+const jwt = require("jsonwebtoken");
+function createToken(id) {
+  return jwt.sign({ _id: id }, process.env.SECRET);
+}
+
 async function signupUser(req, res) {
   const { email, password } = req.body;
 
-  //   --------
   try {
-    // document : row
-
     const user = await User.signup(email, password);
-    res.status(201).json({ email: user.email, id: user._id });
+    const token = createToken(user._id);
+    console.log(token);
+    res.status(201).json({ email, access_token: token });
+  } catch (error) {
+    res.status(400).json(error.message);
+  }
+}
+async function loginUser(req, res) {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
+    const token = createToken(user._id);
+    res.status(200).json({ email, access_token: token });
   } catch (error) {
     res.status(400).json(error.message);
   }
 }
 module.exports = {
   signupUser,
+  loginUser,
 };
--- 24-day/blog-app/backend/models/User.model.js
+++ 24-day/blog-app/backend/models/User.model.js
@@ -36,6 +36,23 @@ UserSchema.statics.signup = async function (email, password) {
   return user;
 };
 
+UserSchema.statics.login = async function (email, password) {
+  if (!email || !password) {
+    throw Error("All fields are required.");
+  }
+
+  const user = await this.findOne({ email });
+  if (!user) {
+    throw Error("Account not found");
+  }
+  const match = await bcrypt.compare(password, user.password);
+  if (!match) {
+    throw Error("Incorrect Password");
+  }
+
+  return user;
+};
+
 const userModel = mongoose.model("User", UserSchema);
 
 module.exports = userModel;
