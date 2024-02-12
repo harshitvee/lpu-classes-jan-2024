b4cc2bb63b522aaa10e0b26e67eaea2a94defd24
Harshit Vashisth <harshitvashisth@gmail.com>
2024-02-12 09:54:04 +0530

    fix patch req

--- 24-day/blog-app/backend/routes/blogs.routes.js
+++ 24-day/blog-app/backend/routes/blogs.routes.js
@@ -5,12 +5,13 @@ const {
   getSingleBlog,
   updateBlog,
   deleteBlog,
+  getMyBlogs,
 } = require("../controllers/blogs.controllers");
 const requireAuth = require("../middlewares/requireAuth");
 
 const router = express.Router(requireAuth);
 router.use(requireAuth);
-
+router.route("/my-blogs").get(getMyBlogs);
 router.route("/").get(getAllBlogs).post(createBlog);
 router.route("/:id").get(getSingleBlog).patch(updateBlog).delete(deleteBlog);
 module.exports = router;
